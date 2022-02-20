b4cadceac0af23660f38c3c4f9a517c75cc8d7fb
Michael Terrell <cwithmichael@gmail.com>
2022-02-19 20:04:18 -0600

    beautify code

--- yelp/static/js/show_map.js
+++ yelp/static/js/show_map.js
@@ -1,21 +1,23 @@
-(function () {
-	'use strict'
-	mapboxgl.accessToken = document.currentScript.getAttribute("tok");
-	var map = new mapboxgl.Map({
-		container: 'show-map',
-		style: 'mapbox://styles/mapbox/streets-v11',
-		center: camp_location,
-		zoom: 9
-	});
-	map.addControl(new mapboxgl.NavigationControl());
-	new mapboxgl.Marker()
-		.setLngLat(camp_location)
-		.setPopup(
-			new mapboxgl.Popup({ offset: 25 })
-				.setHTML(
-					`<h3>${camp_title}</h3>`
-				)
-		)
-		.addTo(map)
+(function() {
+  'use strict'
+  mapboxgl.accessToken = document.currentScript.getAttribute("tok");
+  var map = new mapboxgl.Map({
+    container: 'show-map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: camp_location,
+    zoom: 9
+  });
+  map.addControl(new mapboxgl.NavigationControl());
+  new mapboxgl.Marker()
+    .setLngLat(camp_location)
+    .setPopup(
+      new mapboxgl.Popup({
+        offset: 25
+      })
+      .setHTML(
+        `<h3>${camp_title}</h3>`
+      )
+    )
+    .addTo(map)
 
 })();
